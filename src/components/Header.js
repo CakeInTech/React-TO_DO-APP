7e893323e7981ff2c49b581ae4919a80705403f4
Mohamed Abdulhakim <69153321+MohamedCK@users.noreply.github.com>
2022-09-20 16:08:33 +0200

    Create header

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  const headerStyle = {
+    padding: "20px 0",
+    lineHeight: "1.5em",
+  };
+  return (
+    <header style={headerStyle}>
+      <h1>
+        Todo List
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
