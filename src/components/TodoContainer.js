3b5809e7e43828fdd0227ac97ef7a9e61e5badc6
Mohamed Abdulhakim <69153321+MohamedCK@users.noreply.github.com>
2022-09-22 10:14:07 +0200

    Update the todo stylings

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -3,6 +3,7 @@ import TodosList from "./TodosList";
 import Header from "./Header";
 import InputTodo from "./InputTodo";
 import { v4 as uuidv4 } from "uuid";
+import './TodoItem.module.css';
 
 class TodoContainer extends React.Component {
   state = {
